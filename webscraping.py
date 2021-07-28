5004c3fe95cf6a44ec1a038bfd359c37ac07551d
Melike Karakus <melikekarakus20@gmail.com>
2021-07-28 14:01:56 +0300

    scond commit

--- webscraping.py
+++ webscraping.py
@@ -1,11 +1,12 @@
-import request 
+import requests 
 from bs4 import BeautifulSoup as bs
-
+#beautifulsoup veri kazıma için kullanılan python kütüphanesidir.
 
 github_user = input('Input GitHub User: ')
 url = 'https://github.com/'+github_user
 r = requests.get(url)
-soup = bs(r.content, 'html.parser')
+ #html kodlarını parçalayarak içinden istenen github fotosunu çeker.
+soup = bs(r.content, 'html.parser') 
 profile_image = soup.find('img', {'alt' : 'Avatar'})['src']
-print(profile_image)
+print(profile_image) #fotografı tarayıcıda gösterir
 
